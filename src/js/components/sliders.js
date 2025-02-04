4f745ce33b3728ce525d8a507ffc5485cf6a6d14
Artyushenko Daniil <foxones80@gmail.com>
2025-02-04 23:38:51 +0600

    fixed some

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -15,3 +15,56 @@ new Swiper(".about__slider", {
     nextEl: ".about-next",
   },
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  const resizableSwiper = (
+    breakpoint,
+    swiperClass,
+    swiperSettings,
+    callback
+  ) => {
+    let swiper;
+
+    breakpoint = window.matchMedia(breakpoint);
+
+    const enableSwiper = function (className, settings) {
+      swiper = new Swiper(className, settings);
+
+      if (callback) {
+        callback(swiper);
+      }
+    };
+
+    const checker = function () {
+      if (breakpoint.matches) {
+        return enableSwiper(swiperClass, swiperSettings);
+      } else {
+        if (swiper !== undefined) swiper.destroy(true, true);
+        return;
+      }
+    };
+
+    breakpoint.addEventListener("change", checker);
+    checker();
+  };
+
+  const someFunc = (instance) => {
+    if (instance) {
+      instance.on("slideChange", function (e) {
+        console.log("*** mySwiper.activeIndex", instance.activeIndex);
+      });
+    }
+  };
+
+  resizableSwiper("(max-width: 768px)", ".partners__slider", {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: "auto",
+  });
+
+  resizableSwiper("(max-width: 768px)", ".benefits__slider", {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: "auto",
+  });
+});
