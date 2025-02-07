a0747d2cb115c8ca608bfa96c79b4a17ada48629
Artyushenko Daniil <foxones80@gmail.com>
2025-02-08 03:23:13 +0600

    added 2 pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,7 @@
 import "./_components.js";
 import { burger } from "./functions/burger.js";
+import { Fancybox } from "@fancyapps/ui";
+
+Fancybox.bind('[data-fancybox="gallery1"]', {});
+Fancybox.bind('[data-fancybox="gallery2"]', {});
+Fancybox.bind('[data-fancybox="gallery3"]', {});
